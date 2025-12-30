d62b4b546ae81f5b382dffee66f9a0a51cb7dcb7
juwon <dkreka11@gmail.com>
2025-12-30 16:07:34 +0900

    new ci

--- lib/main.dart
+++ lib/main.dart
@@ -114,8 +114,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int a = 0;
 
   @override
   Widget build(BuildContext context) {
